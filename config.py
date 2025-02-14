c2a032f1261d7676df22d6b907f47efeb7f07dbd
Snupedogs9345 <46502059+Snupedogs9345@users.noreply.github.com>
2025-02-15 02:55:40 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,2 @@
+API_TOKEN = '7707931591:AAF4vIsMLxZMiKQagTDHA8OuInixwuIcEIk' # ТОКЕН
+admin = 593620765 # id администратора
---
+++ keyboard.py
@@ -0,0 +1,31 @@
+from aiogram import types
+
+
+
+menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
+menu.add(
+    types.KeyboardButton('Админ-панель')
+)
+
+adm = types.ReplyKeyboardMarkup(resize_keyboard=True)
+adm.add(
+    types.KeyboardButton('Заблокированные пользователи'),
+    types.KeyboardButton('Заблокировать пользователя'),
+    types.KeyboardButton('Разблокировать пользователя')
+)
+adm.add(types.KeyboardButton('💬 Рассылка'))
+adm.add('⏪ Назад')
+
+back = types.ReplyKeyboardMarkup(resize_keyboard=True)
+back.add(
+    types.KeyboardButton('⏪ Отмена')
+)
+
+
+def fun(user_id):
+    quest = types.InlineKeyboardMarkup(row_width=3)
+    quest.add(
+        types.InlineKeyboardButton(text='💬 Ответить', callback_data=f'{user_id}-ans'),
+        types.InlineKeyboardButton(text='❎ Удалить', callback_data='ignor')
+    )
+    return quest
